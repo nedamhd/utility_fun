81688bc5495ea9a5679a5f9b7890e9bc4bfac89e
Ahad Alizadeh <st.alizadeh@gmail.com>
2020-11-10 22:44:46 +0330

    Update Pips.R

--- Pips.R
+++ Pips.R
@@ -1,10 +1,8 @@
 # my pipes 
-"%f%" <- function(a, b) {
+ "%f%" <- function(a, b) {
      if(is.matrix(b)) {d = dim(b); m =1}
-     if(is.data.frame(b)) {d = dim(b); m =1}
      if(is.vector(b)) {d = length(b); m =0}
-     if(is.list(b)) stop("data must be atomic vector, DF or matrix")
-     b = c(b)
+      b =as.vector(b)
      res = c()
      for(i in 1:length(b))
      res[i]=  paste0(sprintf(paste0("%.",a,"f"), b[i]) )              
