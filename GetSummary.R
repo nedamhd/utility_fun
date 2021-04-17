525b2bbb75da1ccd0ed100252b070736c394640d
Ahad Alizadeh <st.alizadeh@gmail.com>
2021-04-18 02:21:11 +0430

    Update GetSummary.R

--- GetSummary.R
+++ GetSummary.R
@@ -1,7 +1,6 @@
-
-GetSummary <- function(data, dep.quantitative, factor){
-  # data$factor <- as.factor(data[,factor])
- 
+GetSummary <- function(data, dep.quantitative, group){
+  # data$group <- as.group(data[,group])
+  
   ## Pipes ---------------------------------
   "%f%" <- function(a, b) {
     if(is.matrix(b)) {d = dim(b); m =1}
@@ -27,7 +26,7 @@ GetSummary <- function(data, dep.quantitative, factor){
                 significant.number%f%r[3], 
                 ")")
     
-           r2
+    r2
   }
   
   meansdFUN  <- function(x, significant.number = 3) {
@@ -36,7 +35,7 @@ GetSummary <- function(data, dep.quantitative, factor){
     r2 = paste0(significant.number %f% m, 
                 ' \u00B1 ', 
                 significant.number %f% s
-                )
+    )
     
     r2
   }
@@ -73,55 +72,82 @@ GetSummary <- function(data, dep.quantitative, factor){
     
     r2
   }
- #------------------------------------------- 
-
- require(dplyr) 
- 
-
- meansd = data %>% group_by(get(factor))  %>%
+  #------------------------------------------- 
+  
+  require(dplyr) 
+  
+if(!is.null(group)) {  
+  meansd = data %>% group_by(get(group))  %>%
     summarise(across(all_of(dep.quantitative),
                      ~ meansdFUN(.x)), .groups = 'drop')
   
- meanse = data %>% group_by(get(factor))  %>%
+  meanse = data %>% group_by(get(group))  %>%
     summarise(across(all_of(dep.quantitative),
                      ~ meanseFUN(.x)), .groups = 'drop')
   
   
- CI = data %>% group_by(get(factor))  %>%
+  CI = data %>% group_by(get(group))  %>%
     summarise(across(all_of(dep.quantitative),
                      ~ CIFUN(.x)), .groups = 'drop')
- 
-   
- Quantile = data %>% group_by(get(factor))  %>%
+  
+  
+  Quantile = data %>% group_by(get(group))  %>%
     summarise(across(all_of(dep.quantitative),
                      ~ quantileFUN(.x)), .groups = 'drop')
- 
- 
   
- MeanSD         = as.data.frame(t(meansd))
- MeanSE         = as.data.frame(t(meanse))
- CI             = as.data.frame(t(CI))
- MedianQuantile = as.data.frame(t(Quantile))
- 
-   names(MeanSD) = 
-   names(MeanSE) = 
-   names(CI) = 
-   names(MedianQuantile) = 
-     paste0(factor, ": " , c( Quantile[['get(factor)']]))
+  MeanSD         = as.data.frame(t(meansd))
+  MeanSE         = as.data.frame(t(meanse))
+  CI             = as.data.frame(t(CI))
+  MedianQuantile = as.data.frame(t(Quantile))
+  
+  names(MeanSD) = 
+    names(MeanSE) = 
+    names(CI) = 
+    names(MedianQuantile) = 
+    paste0(group, ": " , c( Quantile[['get(group)']]))
+  
+  names(meansd)[1] = names(meanse)[1] = names(CI)[1] = names(Quantile)[1] = group
+ res= list(MeanSD = MeanSD[-1,], MeanSE = MeanSE[-1,], CI =  CI[-1,], MedianQuantile = MedianQuantile[-1,])
+  
+  
+} else {
+  meansd = data %>%  
+    summarise(across(all_of(dep.quantitative),
+                     ~ meansdFUN(.x)), .groups = 'drop')
+  
+  meanse = data %>%  
+    summarise(across(all_of(dep.quantitative),
+                     ~ meanseFUN(.x)), .groups = 'drop')
+  
+  
+  CI = data %>%  
+    summarise(across(all_of(dep.quantitative),
+                     ~ CIFUN(.x)), .groups = 'drop')
+  
+  
+  Quantile = data %>%  
+    summarise(across(all_of(dep.quantitative),
+                     ~ quantileFUN(.x)), .groups = 'drop')  
+  
+  MeanSD         =  data.frame(MeanSD = t(meansd))
+  MeanSE         =  data.frame(MeanSE = t(meanse))
+  CI             =  data.frame(CI = t(CI))
+  MedianQuantile =  data.frame(MedianQuantile = t(Quantile))
+  
+   res = data.frame(cbind(MeanSD, MeanSE, CI, MedianQuantile ))
 
- 
-names(meansd)[1] = names(meanse)[1] = names(CI)[1] = names(Quantile)[1] = factor
- 
-list(MeanSD = MeanSD[-1,], MeanSE = MeanSE[-1,], CI =  CI[-1,], MedianQuantile = MedianQuantile[-1,])
-
+  }
+  
+  
+  res
+  
+  
 }
 
 #-------------------------------
 # d = data.frame(sex=rbinom(10000,1,0.5), age = rnorm(10000), bmi = rnorm(10000))
-# Gkk= GetSummary (data = d, dep.quantitative= c("age", "bmi"), factor = "sex")
+# Gkk= GetSummary (data = d, dep.quantitative= c("age", "bmi"), group = "sex")
+# Gkk= GetSummary (data = d, dep.quantitative= c("age", "bmi"), group =  NULL)
+
 # Gkk
 # Gkk$MeanSD   %>% wd.Table()
-
-
-
-
