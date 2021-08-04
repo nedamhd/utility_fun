fc5b027022303b9543e36a4d0db60784a1e0b3f7
Ahad Alizadeh <st.alizadeh@gmail.com>
2021-08-04 18:42:11 +0430

    Update GLM_Analysis.R
    Add Bayes argument.

--- GLM_Analysis.R
+++ GLM_Analysis.R
@@ -1,63 +1,65 @@
-
-####################
 GLM_Analysis <- R6::R6Class("GLM_Analysis", lock_objects = FALSE, lock_class = FALSE,
                             public         = list(
                               data        = NULL,
                               n.model     = 1,
+                              bayes       = FALSE,
                               # main.table  = data.frame( ),
                               result      = list( ),
-                             # result$models =list(),
+                              # result$models =list(),
                               family      = "gaussian",
                               sepration   = c(),
                               # plot        = NULL,
-                              initialize = function(data, formula, family = "gaussian"){
+                              initialize = function(data, formula, family = "gaussian", bayes = FALSE){
                                 self$data      = data
                                 self$family    = family
+                                self$bayes     = bayes
                                 n.model        = 1
                                 "%+%" <- function(x,y) paste0(x,y)
                                 m1 <- glm(formula = formula, family = family, data = data)
                                 self$sepration[self$n.model] <- !(m1$converged)
                                 m1.ci = private$CI(m1, Family = self$family)$type2.result
-                                if(self$sepration[self$n.model]){
-                                m1 = arm::bayesglm(formula = formula, family = family, data = data)
-                                m1.ci = private$CI(m1, Family = self$family)$type2.result
-                                cat("\nWe re-analyzed using bayesian glm due to Separation.\nIgnore  warning!\n")
-                                names(m1.ci) =c("Effect ","P value ") %+% "Model " %+% n.model %+% "*"
-                                } else {
-                                  names(m1.ci) =c("Effect ","P value ") %+% "Model " %+% n.model
-                                }
-                                 
-                                
-                                 self$result$main.table <- m1.ci
-                                self$result$models <- c( self$result$models,m1)
-                              },
-                              add = function(data = NULL, 
-                                             formula, 
-                                             family = self$family, 
-                                             m = self$result$main.table, 
-                                             n.model = self$n.model + 1 ){
-                                if(is.null(data)) data = self$data
-                                self$n.model = n.model 
-                                "%+%" <- function(x,y) paste0(x,y)
-                                m1 = glm(formula = formula, family = family, data = data)
-                                self$sepration[self$n.model] <- !(m1$converged)
-                                m1.ci = private$CI(m1, Family = self$family)$type2.result
-                                if(self$sepration[self$n.model]){
+                                if(self$sepration[self$n.model] | self$bayes){
                                   m1 = arm::bayesglm(formula = formula, family = family, data = data)
                                   m1.ci = private$CI(m1, Family = self$family)$type2.result
                                   cat("\nWe re-analyzed using bayesian glm due to Separation.\nIgnore  warning!\n")
                                   names(m1.ci) =c("Effect ","P value ") %+% "Model " %+% n.model %+% "*"
                                 } else {
                                   names(m1.ci) =c("Effect ","P value ") %+% "Model " %+% n.model
-                                  }
+                                }
+                                
+                                
+                                self$result$main.table <- m1.ci
+                                self$result$models <- c( self$result$models,m1)
+                              },
+                              add = function(data = NULL, 
+                                             formula, 
+                                             family = self$family, 
+                                             m = self$result$main.table, 
+                                             n.model = self$n.model + 1 , 
+                                             bayes = FALSE){
+                                if(is.null(data)) data = self$data
+                                self$n.model = n.model 
+                                self$bayes = bayes
+                                "%+%" <- function(x,y) paste0(x,y)
+                                m1 = glm(formula = formula, family = family, data = data)
+                                self$sepration[self$n.model] <- !(m1$converged)
+                                m1.ci = private$CI(m1, Family = self$family)$type2.result
+                                if(self$sepration[self$n.model]| self$bayes){
+                                  m1 = arm::bayesglm(formula = formula, family = family, data = data)
+                                  m1.ci = private$CI(m1, Family = self$family)$type2.result
+                                  cat("\nWe re-analyzed using bayesian glm due to Separation.\nIgnore  warning!\n")
+                                  names(m1.ci) =c("Effect ","P value ") %+% "Model " %+% n.model %+% "*"
+                                } else {
+                                  names(m1.ci) =c("Effect ","P value ") %+% "Model " %+% n.model
+                                }
                                 
                                 m = base::merge(m,m1.ci, by="row.names",all=TRUE, suffixes = c("",""))
                                 row.names(m) <- m$Row.names
                                 m$Row.names <- NULL
                                 self$result$main.table <- m
                                 self$result$models <- c( self$result$models,m1)
-
-                                },
+                                
+                              },
                               
                               
                               
@@ -91,73 +93,65 @@ GLM_Analysis <- R6::R6Class("GLM_Analysis", lock_objects = FALSE, lock_class = F
                                               col.names = col.names, ...)
                                   if(!is.null(text))   {writeLines(text , con=datafile)}
                                 }
-                               ),
+                            ),
                             
-                              private = list(
-                                CI = function(M0,round=2, Family=self$family){
-                                  # M0: glm or arm::bayesglm object.
-                                  Re2<- data.frame()
-                                  if( class(M0)[1]== "bayesglm" ){
-                                    # cat("Bayesian method by bayesglm\n\n")
-                                    s.M0=summary(M0)
-                                    M0.s=coef(arm::sim(M0,n=10000))
-                                    CI<- apply(M0.s,2,quantile,
-                                               c(0.025,0.975))
-                                    mean.sim <- apply(M0.s,2,mean)
-                                    
-                                    if(Family == "binomial"){
+                            private = list(
+                              CI = function(M0,round=2, Family=self$family){
+                                # M0: glm or arm::bayesglm object.
+                                Re2<- data.frame()
+                                if( class(M0)[1]== "bayesglm" ){
+                                  # cat("Bayesian method by bayesglm\n\n")
+                                  s.M0=summary(M0)
+                                  M0.s=coef(arm::sim(M0,n=10000))
+                                  CI<- apply(M0.s,2,quantile,
+                                             c(0.025,0.975))
+                                  mean.sim <- apply(M0.s,2,mean)
+                                  
+                                  if(Family == "binomial"){
                                     Re=data.frame (OR= round( exp(mean.sim),round),round(t( exp(CI)),round),P.value=round(s.M0$ coefficients[,4],round+1))
                                     Re2= data.frame("OR"= ( paste0( Re[,1],"(", Re[,2],",", Re[,3], ")")), "P value"= Re[,4])
-                                    }
-                                    
-                                    if(Family == "gaussian"){
-                                      Re=data.frame ("beta"= round( (mean.sim),round),round(t( (CI)),round),P.value=round(s.M0$ coefficients[,4],round+1))
-                                      Re2= data.frame("beta"= ( paste0( Re[,1],"(", Re[,2],",", Re[,3], ")")), "P value"= Re[,4])
-                                    }
-                                    
-                                     row.names(Re2)=row.names(Re)
                                   }
                                   
+                                  if(Family == "gaussian"){
+                                    Re=data.frame ("beta"= round( (mean.sim),round),round(t( (CI)),round),P.value=round(s.M0$ coefficients[,4],round+1))
+                                    Re2= data.frame("beta"= ( paste0( Re[,1],"(", Re[,2],",", Re[,3], ")")), "P value"= Re[,4])
+                                  }
                                   
-                                  if( class(M0)[1]== "glm" ){
-                                    # cat("Classic method by glm\n\n")
-                                    s.M0=summary(M0)
-                                    CI <-  (confint(M0))
-                                    self$sepration[self$n.model] = (self$sepration[self$n.model] | any(is.na(CI)))
-                                    mean.sim <- s.M0$coefficients[,1]
-                                    
-                                    
-                                    if(Family == "binomial"){
+                                  row.names(Re2)=row.names(Re)
+                                }
+                                
+                                
+                                if( class(M0)[1]== "glm" ){
+                                  # cat("Classic method by glm\n\n")
+                                  s.M0=summary(M0)
+                                  CI <-  (confint(M0))
+                                  self$sepration[self$n.model] = (self$sepration[self$n.model] | any(is.na(CI)))
+                                  mean.sim <- s.M0$coefficients[,1]
+                                  
+                                  
+                                  if(Family == "binomial"){
                                     Re=data.frame (OR= round( exp(mean.sim),round),round(( exp(CI)),round),P.value=round(s.M0$ coefficients[,4],round+1))
                                     Re2= data.frame("OR"= (paste0( Re[,1],"(", Re[,2],",", Re[,3], ")")), "P value"= Re[,4])
-                                    }
-                                    
-                                    if(Family == "gaussian"){
-                                      Re=data.frame ("beta"= round( mean.sim,round),round(( (CI)),round),P.value=round(s.M0$ coefficients[,4],round+1))
-                                      Re2= data.frame("beta"= ( paste0( Re[,1],"(", Re[,2],",", Re[,3], ")")), "P value"= Re[,4])
-                                    }
-                                    
-                                    
-                                    row.names(Re2)=row.names(Re)
                                   }
-                                  list(type1.result=Re,type2.result=Re2)
-                                } 
-                                
-                                
-                                
-                                
-                                
-                                
-                              )
+                                  
+                                  if(Family == "gaussian"){
+                                    Re=data.frame ("beta"= round( mean.sim,round),round(( (CI)),round),P.value=round(s.M0$ coefficients[,4],round+1))
+                                    Re2= data.frame("beta"= ( paste0( Re[,1],"(", Re[,2],",", Re[,3], ")")), "P value"= Re[,4])
+                                  }
+                                  
+                                  
+                                  row.names(Re2)=row.names(Re)
+                                }
+                                list(type1.result=Re,type2.result=Re2)
+                              } 
+                              
+                              
+                              
+                              
+                              
+                              
+                            )
 )
-
-
-
-
-
-
-
-
 
 
 
