83f282ef5731dbe8314a52964f325ae64eb34230
Ahad Alizadeh <st.alizadeh@gmail.com>
2021-08-10 21:29:03 +0430

    Update utility_fun.R

--- utility_fun.R
+++ utility_fun.R
@@ -134,34 +134,74 @@ ping.IP <- function() {
  
  #######################
  # Bayesian and classic Confidence interval
- CI = function(M0,round=3){
+ CI = function(M0,round=3, family="binomial"){
   # M0: glm or arm::bayesglm object.
+  if(family=="gaussian"){
+    cat("family: ", family, "\n")
   Re2<- data.frame()
-if( class(M0)[1]== "bayesglm" ){
-  cat("Bayesian method by bayesglm\n\n")
-   s.M0=summary(M0)
-  M0.s=coef(arm::sim(M0,n=10000))
-  CI<- apply(M0.s,2,quantile,
-             c(0.025,0.975))
-  mean.sim <- apply(M0.s,2,mean)
-  Re=data.frame (OR= round( exp(mean.sim),round),round(t( exp(CI)),round),P.value=round(s.M0$ coefficients[,4],round+1))
-  
-  
- Re2= data.frame("OR(95% CI)"= ( paste0( Re[,1],"(", Re[,2],",", Re[,3], ")")), "P value"= Re[,4])
-row.names(Re2)=row.names(Re)
-}
-  
-
-  if( class(M0)[1]== "glm" ){
- cat("Classic method by glm\n\n")
-  s.M0=summary(M0)
-  CI<-  (confint(M0))
-  mean.sim <- s.M0$coefficients[,1]
-  Re=data.frame (OR= round( exp(mean.sim),round),round(( exp(CI)),round),P.value=round(s.M0$ coefficients[,4],round+1))
-  
-  
-  Re2= data.frame("OR(95% CI)"= (paste0( Re[,1],"(", Re[,2],",", Re[,3], ")")), "P value"= Re[,4])
-  row.names(Re2)=row.names(Re)
+  if( class(M0)[1]== "bayesglm" ){
+    cat("Bayesian method by bayesglm\n\n")
+    s.M0=summary(M0)
+    M0.s=coef(arm::sim(M0,n=10000))
+    CI<- apply(M0.s,2,quantile,
+               c(0.025,0.975))
+    mean.sim <- apply(M0.s,2,mean)
+    Re=data.frame (OR= round(  (mean.sim),round),round(t(  (CI)),round),P.value=round(s.M0$ coefficients[,4],round+1))
+    
+    
+    Re2= data.frame("Beta(95% CI)"= ( paste0( Re[,1],"(", Re[,2],",", Re[,3], ")")), "P value"= Re[,4])
+    row.names(Re2)=row.names(Re)
   }
+  
+  
+  if( class(M0)[1] %in% c("glm" , "svyglm")){
+    cat("Classic method by glm\n")
+    s.M0=summary(M0)
+    CI<-  (confint(M0))
+    mean.sim <- s.M0$coefficients[,1]
+    Re=data.frame (OR= round(  (mean.sim),round),round((  (CI)),round),P.value=round(s.M0$ coefficients[,4],round+1))
+    
+    
+    Re2= data.frame("Beta(95% CI)"= (paste0( Re[,1],"(", Re[,2],",", Re[,3], ")")), "P value"= Re[,4])
+    row.names(Re2)=row.names(Re)
+  }
+  }
+  
+ ################################# 
+  if(family=="binomial"){
+    cat("family: ", family, "\n")
+    
+    Re2<- data.frame()
+    if( class(M0)[1]== "bayesglm" ){
+      cat("Bayesian method by bayesglm\n\n")
+      s.M0=summary(M0)
+      M0.s=coef(arm::sim(M0,n=10000))
+      CI<- apply(M0.s,2,quantile,
+                 c(0.025,0.975))
+      mean.sim <- apply(M0.s,2,mean)
+      Re=data.frame (OR= round( exp(mean.sim),round),round(t( exp(CI)),round),P.value=round(s.M0$ coefficients[,4],round+1))
+      
+      
+      Re2= data.frame("OR(95% CI)"= ( paste0( Re[,1],"(", Re[,2],",", Re[,3], ")")), "P value"= Re[,4])
+      row.names(Re2)=row.names(Re)
+    }
+    
+    
+    if( class(M0)[1] %in% c("glm",  "svyglm") ){
+      cat("Classic method by glm\n\n")
+      s.M0=summary(M0)
+      CI<-  (confint(M0))
+      mean.sim <- s.M0$coefficients[,1]
+      Re=data.frame (OR= round( exp(mean.sim),round),round(( exp(CI)),round),P.value=round(s.M0$ coefficients[,4],round+1))
+      
+      
+      Re2= data.frame("OR(95% CI)"= (paste0( Re[,1],"(", Re[,2],",", Re[,3], ")")), "P value"= Re[,4])
+      row.names(Re2)=row.names(Re)
+    }
+  } 
+  
+  
+  
   list(type1.result=Re,type2.result=Re2)
-  }
+}
+
