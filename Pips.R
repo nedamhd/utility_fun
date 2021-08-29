6f0e3fdbb92ee5948428e60f4ce6b223211928d8
Ahad Alizadeh <st.alizadeh@gmail.com>
2021-08-29 18:01:45 +0430

    Update Pips.R

--- Pips.R
+++ Pips.R
@@ -10,3 +10,20 @@
     return(res) 
    }
 "%+%" <- function(a, b) paste0(a, b)
+
+"%num%" <- function(a=NULL,b=NULL) {
+  # Change charecter to numeric. a or b can be null. 
+  if(!is.null(a))   {aname <- deparse(substitute(a))
+  m.temp.12544521 <<- as.numeric(as.character(a)) 
+  te = paste0(aname ,"<- m.temp.12544521")
+  temp = parse(text = te)
+  eval( temp, envir = globalenv())
+  rm(m.temp.12544521, envir = globalenv())}
+  
+  if(!is.null(b))    {bname <- deparse(substitute(b))
+  m.temp.12544522 <<- as.numeric(as.character(b)) 
+  te = paste0(bname ,"<- m.temp.12544522")
+  temp = parse(text = te)
+  eval( temp, envir = globalenv())
+  rm(m.temp.12544522, envir = globalenv())}
+}
