e69a259b19042ce5f14d3e182aa1e6d2b0af77e0
Ahad Alizadeh <st.alizadeh@gmail.com>
2021-08-31 00:34:44 +0430

    Create Correlations.R

---
+++ Correlations.R
@@ -0,0 +1,9 @@
+corr = function(data){
+source("https://raw.githubusercontent.com/ahadalizadeh/utility_fun/master/Pips.R")
+library("Hmisc")
+res2 <- rcorr(as.matrix(data))
+pp = ((2%f%res2$r) %+% " (") %+% ((3 %f% res2$P) %+% ")")
+colnames(pp)      =  colnames(res2$r )  
+row.names(pp)  =  row.names(res2$r ) 
+list(combine = pp, r = res2$r, p = res2$P)
+}
