57ffe2af70bf4c4022011cc7c5ab3e2747e06e08
Ahad Alizadeh <st.alizadeh@gmail.com>
2021-10-04 12:50:47 +0330

    Update PSQI.questionnaire.R

--- PSQI.questionnaire.R
+++ PSQI.questionnaire.R
@@ -1,39 +1,64 @@
-
-
-PSQI.questionnaire <- function(data, questions){
+PSQI.questionnaire = 
+function(data, questions){
   # Pittsburgh Sleep Quality Index
   # questions: name or index of questions 1:18, respectivly.
   if(length(questions) != 18) stop("Number of questions must be exactly 18.")
   for (i in c(6:15,18))  
     if( !all( c(data[[questions[i]]])%in% c(NA,0:3))) stop(paste0("value of questions ", i," must be exactly 0:3"))
-  J = data
+ 
   
-  names(J[,questions]) = paste0("q", 1:18)
+  J = data[,questions]
+  names(J) = paste0("q", 1:18)
   data$keifiyat.zehni.khab = J$q18
-  J$q2.cat = c(cut(J$q2 , breaks = c(0,15,30,60)))-1
+  
+  J$q2.cat = NA  
+  J$q2.cat[which( J$q2<=1000)] =  3
+  J$q2.cat[which( J$q2<=60)] =  2
+  J$q2.cat[which( J$q2<=30)] =  1
+  J$q2.cat[which( J$q2<=15)] =  0
   data$Takhir.dar.be.khab.raftan = J$q2.cat + c(J$q6)
-  J$q2.cat = NULL
-  data$Tole.moddat.khab = 4-c(cut(J$q4 , breaks = c(0,5,6,7,1000))) 
+  
+  data$Tole.moddat.khab = NA 
+  data$Tole.moddat.khab[which( J$q2<=1000)] =  0
+  data$Tole.moddat.khab[which( J$q2<=7)]    =  1
+  data$Tole.moddat.khab[which( J$q2<=6)]    =  2
+  data$Tole.moddat.khab[which( J$q2<=5)]    =  3
+
+  
   J$q1.change = J$q1 - 12
   J$q1.change [which(abs(J$q1.change) > 6)] =J$q1[which(abs(J$q1.change) > 6)]
-  data$Karayi = (4-c(cut(J$q4/abs((J$q1.change ) - J$q3)*100, breaks = c(0,65,75,85,101))) )
-  J$q1.change = NULL
-  data$Ekhtelale.khab = c(cut(with(J,q6+q7+q8+q9+q10+q11+q12+q13+q14+q15), breaks = c(-10,0,9,18,27)))-1
+  
+  data$Karayi = NA
+  data$Karayi[which( (J$q4/abs((J$q1.change ) - J$q3)*100) <=101)] =  0
+  data$Karayi[which( (J$q4/abs((J$q1.change ) - J$q3)*100) <=85)] =  1
+  data$Karayi[which( (J$q4/abs((J$q1.change ) - J$q3)*100) <=75)] =  2
+  data$Karayi[which( (J$q4/abs((J$q1.change ) - J$q3)*100) <=65)] =  3
+
+  data$Ekhtelale.khab = NA
+  data$Ekhtelale.khab[which( with(J,q6+q7+q8+q9+q10+q11+q12+q13+q14+q15) <=27)]   =  3
+  data$Ekhtelale.khab[which( with(J,q6+q7+q8+q9+q10+q11+q12+q13+q14+q15) <=18)]   =  2
+  data$Ekhtelale.khab[which( with(J,q6+q7+q8+q9+q10+q11+q12+q13+q14+q15) <=9)]    =  1
+  data$Ekhtelale.khab[which( with(J,q6+q7+q8+q9+q10+q11+q12+q13+q14+q15) <=0)]    =  0
+
+  # data$Ekhtelale.khab = c(cut(with(J,q6+q7+q8+q9+q10+q11+q12+q13+q14+q15), breaks = c(-10,0,9,18,27)))-1
   data$Masraf.daru.khabavar = J$q15 
   J$Ekhtelale.amalkarde.rozane.temp = with(J,q16+q17)
-  data$Ekhtelale.amalkarde.rozane = c(cut(J$Ekhtelale.amalkarde.rozane.temp, breaks = c(-10,0,2,4,6)))-1
-  J$Ekhtelale.amalkarde.rozane.temp = NULL
   
-  data$PSQI = with(data, keifiyat.zehni.khab+ 
-                     Takhir.dar.be.khab.raftan+
-                     Tole.moddat.khab+
-                     Karayi+
-                     Ekhtelale.khab+
-                     Masraf.daru.khabavar+
+  data$Ekhtelale.amalkarde.rozane = NA
+  data$Ekhtelale.amalkarde.rozane[which( J$Ekhtelale.amalkarde.rozane.temp <=  6)]   =  3
+  data$Ekhtelale.amalkarde.rozane[which( J$Ekhtelale.amalkarde.rozane.temp <=  4)]   =  2
+  data$Ekhtelale.amalkarde.rozane[which( J$Ekhtelale.amalkarde.rozane.temp <=  2)]   =  1
+  data$Ekhtelale.amalkarde.rozane[which( J$Ekhtelale.amalkarde.rozane.temp <=  0)]   =  0
+
+
+  data$PSQI = with(data, keifiyat.zehni.khab + 
+                     Takhir.dar.be.khab.raftan +
+                     Tole.moddat.khab +
+                     Karayi +
+                     Ekhtelale.khab +
+                     Masraf.daru.khabavar +
                      Ekhtelale.amalkarde.rozane
                    
   )
   data
 }
-
-# J= PSQI.questionnaire (data=J, questions = paste0("q",1:18))
