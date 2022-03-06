4922ee42a5cf1a0a0918889734cc7d0b8139d1eb
Ahad Alizadeh <st.alizadeh@gmail.com>
2022-03-06 23:34:50 +0330

    Update DASS-21.R

--- DASS-21.R
+++ DASS-21.R
@@ -1,18 +1,37 @@
 DASS = 
   function(data, vars = NULL){
+    require(dplyr)
     if(is.null(vars)){
-      if(dim(data)[20 != 21]) stop("The number of variables must be 21.")
+      if(dim(data)[2] != 21) stop("The number of variables must be 21.")
+      data = data %>% mutate_if(is.character, as.numeric)
       Depression = rowSums(data[,c(21,17,16,13,10,5,3)])
       Anxiety    = rowSums(data[,c(20,19,15,9,7,4,2)])
       Stress     = rowSums(data[,c(18,14,12,11,8,6,1)])
    } else {
      if(length(vars) != 21) stop("The number of variables must be 21.")
         data = data[,vars]
-         Depression = rowSums(data[,c(21,17,16,13,10,5,3)])
+        data = data %>% mutate_if(is.character, as.numeric)
+        Depression = rowSums(data[,c(21,17,16,13,10,5,3)])
         Anxiety    = rowSums(data[,c(20,19,15,9,7,4,2)])
         Stress     = rowSums(data[,c(18,14,12,11,8,6,1)])
        }
    
  data.frame(Depression, Anxiety, Stress)
  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
