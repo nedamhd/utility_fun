268873568b179ca6a106a96734296bc5840853db
Ahad Alizadeh <st.alizadeh@gmail.com>
2022-03-06 11:30:55 +0330

    Update Ferquency.R

--- Ferquency.R
+++ Ferquency.R
@@ -1,11 +1,26 @@
-Ferquency  =  function(data, x, group = NULL){
+# source("https://raw.githubusercontent.com/ahadalizadeh/utility_fun/master/Ferquency.R")
+
+Ferquency  =  function(data, x = NULL, group = NULL){
+   require(dplyr)
+
+  if(is.null(x)) {
+    data2 = data %>% select(where(is.factor))
+    x = names(data2)
+    if(!is.null(group)){
+    x = x[which(group != x)]
+   
+    data =  data[,c(x, group)]
+   
+  }
+    }
+ 
+ 
   .x = data[,x]  
   if(!is.null(group)) .group = data[,group] else .group = rep(1, length(.x))
-  require(dplyr)
   percentage = function(.x, digits = 2 ) {
     t.x = table(.x)
     p.t.x = round(prop.table(t.x )*100, digits)
-    p.t = data.frame(t(paste0(t.x,"(",p.t.x,")")))
+    p.t = data.frame(t(paste0(t.x," (",p.t.x,")")))
     names(p.t) =  names(t.x)
     p.t
   }
@@ -14,17 +29,22 @@ Ferquency  =  function(data, x, group = NULL){
     data %>% group_by_at (group )%>%  summarise_at(   x  ,percentage )
   )
   )
-  dd
+ if(dim(dd)[2] == 1)
+   names(dd) = "count_percent"
+ 
+ dd
 }
 
 cat('
 Example:
 data = data.frame("sex" = rbinom(1000,1,0.5),
-                  "edu"= rbinom(1000,1,0.5), 
-                  "job"= rbinom(1000,1,0.5), 
-                  "pulp"= rbinom(1000,1,0.5), 
+                  "edu"= rbinom(1000,1,0.5),
+                  "job"= rbinom(1000,1,0.5),
+                  "pulp"= rbinom(1000,1,0.5),
                   "marital"= rbinom(1000,1,0.5))
 x = c("sex","edu", "job"  )
 group = c("pulp", "marital")
-Ferquency  (data, x, group = group) 
+Ferquency  (data, x, group = group)
 ')
+
+--
