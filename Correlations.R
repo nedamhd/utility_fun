f75bfc03d8e351231172ed686fd73dfeca10e41a
Ahad Alizadeh <st.alizadeh@gmail.com>
2023-01-11 21:11:26 +0330

    Update Correlations.R

--- Correlations.R
+++ Correlations.R
@@ -1,9 +1,10 @@
-correlations = function(data){
-source("https://raw.githubusercontent.com/ahadalizadeh/utility_fun/master/Pips.R")
-library("Hmisc")
-res2 <- rcorr(as.matrix(data))
-pp = ((2%f%res2$r) %+% " (") %+% ((3 %f% res2$P) %+% ")")
-colnames(pp)      =  colnames(res2$r )  
-row.names(pp)  =  row.names(res2$r ) 
-list(combine = pp, r = res2$r, p = res2$P)
+correlations = function(data, type = c("pearson", "spearman")){
+  source("https://raw.githubusercontent.com/ahadalizadeh/utility_fun/master/Pips.R")
+  library("Hmisc")
+  res2 <- rcorr(as.matrix(data),type = type)
+  pp = ((2%f%res2$r) %+% " (") %+% ((3 %f% res2$P) %+% ")")
+  colnames(pp)      =  colnames(res2$r )  
+  row.names(pp)  =  row.names(res2$r ) 
+  list(combine = pp, r = res2$r, p = res2$P)
 }
+
